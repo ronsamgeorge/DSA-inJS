24cb10f283e61f7d4c9e15174223bf755b2552e1
Ronald George <ronaldsamgeorge@gmail.com>
2023-04-04 11:41:29 +0530

    Implementation: cyclic sort

---
+++ sorting/cyclicSort.js
@@ -0,0 +1,21 @@
+// pattern : when given numbers of range 1 to n in array of length n
+
+const cyclicSort = (arr) => {
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] - 1 === i) {
+      i += 1;
+      continue;
+    }
+
+    let temp = arr[i];
+    arr[i] = arr[temp - 1];
+    arr[temp - 1] = temp;
+  }
+
+  return arr;
+};
+
+const arr = [3, 4, 2, 1, 5, 6];
+console.log("initial : ", arr);
+console.log("final : ", cyclicSort(arr));
