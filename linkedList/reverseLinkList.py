0d2c59f0b0811be64880e92aefce7ef5bb44aa30
Ronald George <ronaldsamgeorge@gmail.com>
2023-07-15 12:42:15 +0530

    Reverse Linked List Using recursion

--- linkedList/reverseLinkList.py
+++ linkedList/reverseLinkList.py
@@ -23,3 +23,29 @@ class Solution:
         curr.next = prev
         head = curr
         return head
+
+
+# using recursion
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head or not head.next:  # empty or one element present , return
+            return head
+
+        # create dummy link to the new head
+        dummy = ListNode()
+        dummy.next = head
+        self.reverseNodes(head, dummy)
+        return dummy.next
+
+    def reverseNodes(self, head, dummy):
+
+        if head.next == None:
+            dummy.next = head
+            return head
+
+        prev = self.reverseNodes(head.next, dummy)
+
+        prev.next = head
+        head.next = None
+        return head
