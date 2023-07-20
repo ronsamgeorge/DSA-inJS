8e6fe6322fc3f73f5bd50503c96f8a5358606fbc
Ronald George <ronaldsamgeorge@gmail.com>
2023-07-20 12:42:42 +0530

    Stack : remove star from strings

---
+++ stack/removeStarsfromString.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/removing-stars-from-a-string/description/
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+
+        stack = []
+
+        for ch in s:
+            if ch == "*":
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return ''.join(stack)
