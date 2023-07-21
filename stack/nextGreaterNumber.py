8570ebc7fba9f8da015ddd6aed5d8c7ab5ee2648
Ronald George <ronaldsamgeorge@gmail.com>
2023-07-21 22:29:10 +0530

    Stack : next greater element

--- stack/nextGreaterNumber.py
+++ stack/nextGreaterNumber.py
@@ -1,30 +1,25 @@
+# https://leetcode.com/problems/next-greater-element-i/description/
+
+# use monotonic decreasing stack
+
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-        next_greater = {}
+        map_num1 = {}
         mono_stack = []
-        res = []
+        res = [-1 for x in range(len(nums1))]  # initialize res with -1
 
-        for i in range(len(nums2)-1, -1, -1):
+        # create hash map with num1 value as key  and index has value
+        for index, num in enumerate(nums1):
+            map_num1[num] = index
 
-            if len(mono_stack) == 0:
-                next_greater[nums2[i]] = -1
-                mono_stack.append(nums2[i])
-
-            elif mono_stack[-1] > nums2[i]:
-                next_greater[nums2[i]] = mono_stack[-1]
-                while len(mono_stack) > 0 and not mono_stack[-1] < nums2[i]:
-                    mono_stack.pop()
-                mono_stack.append(nums2[i])
-
-            else:
-                next_greater[nums2[i]] = -1
-                while len(mono_stack) != 0:
-                    mono_stack.pop()
-
-                mono_stack.append(nums2[i])
-
-        for num in nums1:
-            res.append(next_greater[num])
+        for num in nums2:
+            # as long as the current element is greater pop()
+            while mono_stack and num > mono_stack[-1]:
+                temp = mono_stack.pop()
+                if temp in map_num1:
+                    # if what was popped is in num1 update res at that index
+                    res[map_num1[temp]] = num
+            mono_stack.append(num)             # insert num at the top of stack
 
         return res
