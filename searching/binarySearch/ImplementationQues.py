66684f9ed0e8bbfbe3b1175aefa9e34df9a668d1
Ronald George <ronaldsamgeorge@gmail.com>
2023-07-26 22:34:13 +0530

    Binary search : Implement Ques

---
+++ searching/binarySearch/ImplementationQues.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/binary-search
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binarySearch(nums, target, 0, len(nums)-1)
+
+    def binarySearch(self, nums, target, start, end):
+
+        if start > end:
+            return -1
+
+        mid = (start+end) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] < target:
+            return self.binarySearch(nums, target, mid+1, end)
+        else:
+            return self.binarySearch(nums, target, start, mid-1)
