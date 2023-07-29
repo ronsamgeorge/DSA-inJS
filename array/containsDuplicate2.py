cf8852b376d549124e10741b54978a3dd623b315
Ronald George <ronaldsamgeorge@gmail.com>
2023-07-29 20:08:49 +0530

    Array : Contains Duplicate 2

---
+++ array/containsDuplicate2.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/contains-duplicate-ii
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        res_hash = {}
+
+        for index in range(len(nums)):
+            if nums[index] in res_hash:
+                if abs(res_hash[nums[index]] - index) <= k:
+                    return True
+
+            res_hash[nums[index]] = index
+
+        return False
